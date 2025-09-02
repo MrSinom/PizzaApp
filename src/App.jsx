1757a655caabdf75d65249255fc2fe8b880e3fb2
Szymon Gebala <gebala.simon@gmail.com>
2025-09-03 00:26:43 +0200

    creating browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from './UI/Home'
+import Menu from './fetures/menu/Menu'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Home/>
+  },
+  {
+    path:'/menu',
+    element: <Menu /> 
+  }
+])
 function App() {
-  return<div>
-    Hello vite
-  </div>
+  return <RouterProvider router= {router}/>
 }
 
 export default App;
